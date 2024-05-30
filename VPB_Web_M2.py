56395c5269ab3448a37943f5209d26767a9236cc
yenlh8 <lehoangyen1986@gmail.com>
2024-05-30 23:44:21 +0700

    VPB_Web_M2.py

--- VPB_Web_M2.py
+++ VPB_Web_M2.py
@@ -11,8 +11,8 @@ st.write("Write customer's overall requirements for products here. In case compe
 
 # ///////////
 
-# cus_info_table = pd.read_excel(r'D:\VPB Hackathon\SampleData2\Model2_input_data.xlsx').set_index('CusID')
-cus_info_table = pd.read_excel('https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fyenlh8%2FVPB_Hackathon%2Fmain%2FModel2_input_data.xlsx&wdOrigin=BROWSELINK', engine='openpyxl').set_index('CusID')
+cus_info_table = pd.read_excel('Model2_input_data.xlsx').set_index('CusID')
+# cus_info_table = pd.read_excel('https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fyenlh8%2FVPB_Hackathon%2Fmain%2FModel2_input_data.xlsx&wdOrigin=BROWSELINK', engine='openpyxl').set_index('CusID')
 
 CusID1, CusName, empty_col = st.columns(3)
 CusID = CusID1.number_input('CusID', value = None, min_value = 9000, max_value = 9999, format = '%d')
@@ -62,8 +62,8 @@ with st.form('Constraints/Requirements Input'):
 
 # ///////////////////////////////
 
-# TD_Rate_Standard = pd.read_excel(r'D:\VPB Hackathon\SampleData2\TD_Rate_Standard.xlsx').set_index('Months_Num')
-TD_Rate_Standard = pd.read_excel('https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fyenlh8%2FVPB_Hackathon%2Fmain%2FTD_Rate_Standard.xlsx&wdOrigin=BROWSELINK', engine='openpyxl').set_index('Months_Num')
+TD_Rate_Standard = pd.read_excel('TD_Rate_Standard.xlsx').set_index('Months_Num')
+# TD_Rate_Standard = pd.read_excel('https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fyenlh8%2FVPB_Hackathon%2Fmain%2FTD_Rate_Standard.xlsx&wdOrigin=BROWSELINK', engine='openpyxl').set_index('Months_Num')
 
 
 # calc_button = st.button('Click me!')
